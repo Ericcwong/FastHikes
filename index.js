63211d2fcf113bc01c7e90255ce580e81d70ed0c
Erik Huynh <Erik.W.Huynh@gmail.com>
2020-01-04 09:48:38 -0700

    Used opencageAPI to turn lat.long to city name

--- index.js
+++ index.js
@@ -4,10 +4,17 @@ $("#geoloc-btn").on("click", function(event){
         navigator.geolocation.getCurrentPosition(function(showPosition){
             var lat = showPosition.coords.latitude;
             var long = showPosition.coords.longitude;
-            localStorage.setItem("lat", lat);
-            localStorage.setItem("long", long);
-            $("#location-input").val(`${lat}, ${long}`);
-        });
+            var queryURL = `https://api.opencagedata.com/geocode/v1/json?q=${lat}+${long}&key=bd7510ab6530475cbd7652e1b7b4f5da`;
+            console.log(`queryURL: ${queryURL}`);
+
+            $.ajax({
+            url: queryURL,
+            method: "GET"
+                }).then(function(response) {
+                    var city = response.results[0].components.city;
+                    $("#location-input").val(city);
+                });
+            });
     } else {
     console.log("geoLocation not supported on browser")
     }
