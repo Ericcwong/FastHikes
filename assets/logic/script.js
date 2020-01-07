f8157044583dcadfeb33df418b8661f3e57426bd
Dan D <degeare28@gmail.com>
2020-01-06 18:40:17 -0700

    layout to js

--- assets/logic/script.js
+++ assets/logic/script.js
@@ -6,6 +6,22 @@ $(document).ready(function() {
   //FUNCTION TO SEND API REQUEST TO MAP API
   //FUNCTION TO CHANGE ADDRESS/ZIP INTO LAT ANG LONG
   //FUNCTION TO SEND API REQUEST TO ALL TRAILS API
+
+  function searchTrail(lat, long){
+    
+           
+            var queryURL = `https://www.hikingproject.com/data/get-trails?lat=${lat}&lon=${long}&maxDistance=10&key=200651467-de1e6529db5dab5088baa201faae4247`;
+            console.log(`queryURL: ${queryURL}`);
+
+            $.ajax({
+            url: queryURL,
+            method: "GET"
+                }).then(function(response) {
+                    var city = response.results[0].components.city;
+                    
+                });
+            });
+  }
   //FUNCTION TO FILTER ALL TRAILS RESULTS BY TIME
   //FUNCTION TO SEND HIKE LOCATIONS TO MAP API
   //FUNCTION TO CALCULATE TIME TO TRAIL AND BACK
