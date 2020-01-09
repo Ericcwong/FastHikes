9ad07c19707e3e1bfb866e1df9dfa69031c00428
Dan D <degeare28@gmail.com>
2020-01-08 21:16:53 -0700

    main logic loop done

--- assets/logic/script.js
+++ assets/logic/script.js
@@ -3,35 +3,36 @@ $(document).ready(function() {
   userlat = localStorage.getItem("lat");
   userlong = localStorage.getItem("long");
   userDuration = localStorage.getItem("duration");
+  queryURL = `https://www.hikingproject.com/data/get-trails?lat=${userlat}&lon=${userlong}&key=200651467-de1e6529db5dab5088baa201faae4247`;
   var trailStats = [];
   var travelTimes = [];
-  var travelDuration;
-
-  var travelHours = 0;
+  var travelDuration = [];
+  var totalTime = [];
+  var mainData = [];
 
   console.log(userlat, userlong, userDuration);
 
   //Call Alltrails API via lat and long
-  queryURL = `https://www.hikingproject.com/data/get-trails?lat=${userlat}&lon=${userlong}&key=200651467-de1e6529db5dab5088baa201faae4247`;
   $.ajax({
     url: queryURL,
     method: "GET"
-  }).then(function(response) {
-    var results = response.trails;
-    console.log(results);
-    //loop through the returned hike object and and get lat, long, and length of each hike
+  }).then(dataLoop);
 
-    for (var i = 0; i < results.length; i++) {
+  //loop through the returned hike object and and get lat, long, and length of each hike
+  function dataLoop(x) {
+    mainData = x.trails;
+    y = x.trails;
+    console.log(y);
+    for (var i = 0; i < y.length; i++) {
       var object = {
-        lat: results[i].latitude,
-        long: results[i].longitude,
-        len: results[i].length
+        lat: y[i].latitude,
+        long: y[i].longitude,
+        len: (y[i].length / 3) * 3600
       };
       trailStats.push(object);
     }
     console.log(trailStats);
     //call travelmatrix API against each returned hike with origin as user input lat and long and destination as hike lat long
-
     //Google JS API Stuff
     for (var i = 0; i < trailStats.length; i++) {
       var origin1 = new google.maps.LatLng(userlat, userlong);
@@ -56,18 +57,31 @@ $(document).ready(function() {
         },
         callback
       );
-
-      function callback(response, status) {
-        travelDuration =
-          response.rows["0"].elements["0"].duration_in_traffic.value;
-        //travelHours = (travelDuration / 3600).toPrecision(2);
-        //console.log(travelDuration);
-        travelTimes.push(travelDuration);
-      }
     }
-    console.log(travelTimes);
-    console.log((trailStats[0].len / 3) * 3600 + travelTimes[0]);
-  });
+
+    function callback(response) {
+      travelDuration =
+        response.rows["0"].elements["0"].duration_in_traffic.value;
+      travelTimes.push(travelDuration);
+      console.log(travelTimes);
+      if (travelTimes.length == 10) {
+        for (var i = 0; i < travelTimes.length; i++) {
+          allTime = trailStats[i].len + travelTimes[i];
+          totalTime.push(allTime);
+          console.log(totalTime);
+          console.log(mainData);
+          userDurationSeconds = userDuration * 3600;
+          console.log(userDurationSeconds);
+          if (totalTime[i] > userDurationSeconds) {
+            mainData.splice([i], 1);
+          }
+        }
+      }
+      console.log(mainData);
+      mainObject = JSON.stringify(mainData);
+      localStorage.setItem("mainObject", mainObject);
+    }
+  }
 });
 
 //use returned duration from google, + hike length to filter the returned hikes against the user input duration.
