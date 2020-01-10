665b54cdf8e6a9f560299e476bad016b2e7266f5
Erik Huynh <Erik.W.Huynh@gmail.com>
2020-01-10 12:47:38 -0700

    Combined script.js into hikecards.js

--- assets/logic/hikeCards.js
+++ assets/logic/hikeCards.js
@@ -1,29 +1,118 @@
-var obj = JSON.parse(localStorage.getItem("mainObject"));
-
 $(document).ready(function(){
- for (var i=0; i<obj.trails.length; i++){   
-    createCard(i);
- }
 
-function createCard(i){
-    var card = $(`
-    <div class="hikeCard">
-    <div class="image">
-    <img src="${obj.trails[i].imgMedium}" alt="Image" class="hikeImage" />
-    </div>
-    <div class="hikeTitle"><span class="cardTitles">Title:</span> ${obj.trails[i].name}</div>
-    <div class="hikeDuration"><span class="cardTitles">Hike Length:</span> ${obj.trails[i].length} Miles </div>
-    <div class="hikeDetails">
-    <span class="cardTitles">Rating: </span>${obj.trails[i].stars} (${obj.trails[i].starVotes}) &nbsp;&nbsp;&nbsp;
-    <span class="cardTitles">Difficulty:</span> ${obj.trails[i].difficulty} &nbsp;&nbsp;&nbsp;
-    <span class="cardTitles">Elevation:</span> ${obj.trails[i].ascent} ft
-    <br><span class="cardTitles">Details:</span> ${obj.trails[i].summary}
-    <br><span class="cardTitles">Conditions:</span> ${obj.trails[i].conditionDetails}
-    </div>
-  </div>`);
-  $("#resultsArea").append(card);
+  //Get Lat, long, and duration from local memory
+  userlat = localStorage.getItem("lat");
+  userlong = localStorage.getItem("long");
+  userDuration = localStorage.getItem("duration");
+  queryURL = `https://www.hikingproject.com/data/get-trails?lat=${userlat}&lon=${userlong}&key=200651467-de1e6529db5dab5088baa201faae4247`;
+  var trailStats = [];
+  var travelTimes = [];
+  var travelDuration = [];
+  var totalTime = [];
+  var mainData = [];
 
-}
+  console.log(userlat, userlong, userDuration);
+  //test comment
+  //Call Alltrails API via lat and long
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(dataLoop)
+  .then(init);
+
+  //loop through the returned hike object and and get lat, long, and length of each hike
+  function dataLoop(x) {
+    mainData = x;
+    y = x.trails;
+    console.log(y);
+    for (var i = 0; i < y.length; i++) {
+      var object = {
+        lat: y[i].latitude,
+        long: y[i].longitude,
+        len: (y[i].length / 3) * 3600
+      };
+      trailStats.push(object);
+    }
+    // console.log(trailStats);
+    //call travelmatrix API against each returned hike with origin as user input lat and long and destination as hike lat long
+    //Google JS API Stuff
+    for (var i = 0; i < trailStats.length; i++) {
+      var origin1 = new google.maps.LatLng(userlat, userlong);
+      var destinationB = new google.maps.LatLng(
+        trailStats[i].lat,
+        trailStats[i].long
+      );
+
+      var service = new google.maps.DistanceMatrixService();
+      service.getDistanceMatrix(
+        {
+          origins: [origin1],
+          destinations: [destinationB],
+          travelMode: "DRIVING",
+          drivingOptions: {
+            departureTime: new Date(Date.now()),
+            trafficModel: "optimistic"
+          },
+          unitSystem: google.maps.UnitSystem.IMPERIAL,
+          avoidHighways: false,
+          avoidTolls: false
+        },
+        callback
+      );
+    }
+
+    function callback(response) {
+      travelDuration =
+        response.rows["0"].elements["0"].duration_in_traffic.value;
+      travelTimes.push(travelDuration);
+      // console.log(travelTimes);
+      if (travelTimes.length == 10) {
+        for (var i = 0; i < travelTimes.length; i++) {
+          allTime = trailStats[i].len + travelTimes[i];
+          totalTime.push(allTime);
+          // console.log(totalTime);
+          // console.log(mainData);
+          userDurationSeconds = userDuration * 3600;
+          // console.log(userDurationSeconds);
+          if (totalTime[i] > userDurationSeconds) {
+            mainData.trails.splice([i], 1);
+          }
+        }
+      }
+      console.log(mainData);
+      mainObject = JSON.stringify(mainData);
+      localStorage.setItem("mainObject", mainObject);
+    }
+  }
+  var obj;
+
+  function init(){
+    obj = JSON.parse(localStorage.getItem("mainObject"));
+    for (var i=0; i<obj.trails.length; i++){   
+        createCard(i);
+    }
+  }
+
+
+  function createCard(i){
+      var card = $(`
+      <div class="hikeCard">
+      <div class="image">
+      <img src="${obj.trails[i].imgMedium}" alt="Image" class="hikeImage" />
+      </div>
+      <div class="hikeTitle"><span class="cardTitles">Title:</span> ${obj.trails[i].name}</div>
+      <div class="hikeDuration"><span class="cardTitles">Hike Length:</span> ${obj.trails[i].length} Miles </div>
+      <div class="hikeDetails">
+      <span class="cardTitles">Rating: </span>${obj.trails[i].stars} (${obj.trails[i].starVotes}) &nbsp;&nbsp;&nbsp;
+      <span class="cardTitles">Difficulty:</span> ${obj.trails[i].difficulty} &nbsp;&nbsp;&nbsp;
+      <span class="cardTitles">Elevation:</span> ${obj.trails[i].ascent} ft
+      <br><span class="cardTitles">Details:</span> ${obj.trails[i].summary}
+      <br><span class="cardTitles">Conditions:</span> ${obj.trails[i].conditionDetails}
+      </div>
+    </div>`);
+    $("#resultsArea").append(card);
+
+  }
 
 });
 
--- assets/logic/index.js
+++ assets/logic/index.js
@@ -39,6 +39,6 @@ $("#input-form").on("submit", function(event){
             var long = response.results[0].geometry.lng;
             localStorage.setItem("lat", lat);
             localStorage.setItem("long", long);
-            window.location.href = "/FastHikes/results";
+            window.location.href = "results.html";
         });
 });
