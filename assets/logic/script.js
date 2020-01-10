aeba88339cc6c6d2c38bfdcd7be4d82b112e14cc
Erik Huynh <Erik.W.Huynh@gmail.com>
2020-01-10 13:18:01 -0700

    Removed script.js, reset href to work on github

--- assets/logic/script.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-  
-});
-
-//use returned duration from google, + hike length to filter the returned hikes against the user input duration.
-
-//trim the hike object to only contain the valid hikes.
